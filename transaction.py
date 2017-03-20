c7a10e8cbb403788b045fad34753128c13f38882
Nitin <nitin.tiwari@fulfil.io>
2017-03-20 19:50:43 +0530

    Remove provider from add profile view

--- transaction.py
+++ transaction.py
@@ -988,19 +988,9 @@ class AddPaymentProfileView(BaseCreditCardViewMixin, ModelView):
         domain=[('party', '=', Eval('party'))],
         depends=['party']
     )
-    provider = fields.Selection('get_providers', 'Provider', required=True)
     gateway = fields.Many2One(
         'payment_gateway.gateway', 'Gateway', required=True,
-        domain=[('provider', '=', Eval('provider'))],
-        depends=['provider']
     )
-
-    @classmethod
-    def get_providers(cls):
-        """
-        Return the list of providers who support credit card profiles.
-        """
-        return []
 
 
 class AddPaymentProfile(Wizard):
@@ -1091,7 +1081,7 @@ class AddPaymentProfile(Wizard):
         If return_profile is set to True in the context, then the created
         profile is returned.
         """
-        method_name = 'transition_add_%s' % self.card_info.provider
+        method_name = 'transition_add_%s' % self.card_info.gateway.provider
         if Transaction().context.get('return_profile'):
             return getattr(self, method_name)()
         else:
